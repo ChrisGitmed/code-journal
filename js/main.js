1878ce386225abf2f6ede2371fef3ee675cbf256
Chris Gitmed <cgitmed@gmail.com>
2020-10-26 15:15:08 -0700

    Listen for 'submit' events on the form'

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var avatarUrlInput = document.querySelector('#avatar-URL');
 var avatarImage = document.querySelector('img');
+var form = document.querySelector('form');
 
 function changePicture(event) {
   avatarImage.src = event.target.value;
 }
 
+function submitValues(event) {
+
+}
+
 avatarUrlInput.addEventListener('input', changePicture);
+form.addEventListener('submit', submitValues);
