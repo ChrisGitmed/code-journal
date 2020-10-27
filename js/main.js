fb9823d2ee31d4bdaab16b2bb2f7f8b9c4c94249
Chris Gitmed <cgitmed@gmail.com>
2020-10-27 10:34:13 -0700

    Define a JavaScript function that takes the profile data model and returns a DOM tree that matches the HTML

--- js/main.js
+++ js/main.js
@@ -22,6 +22,46 @@ function setItemsInStorage(event) {
   localStorage.setItem('code-journal', dataJSON);
 }
 
+function getProfileDataInDOM(data) {
+  var newProfile = document.createElement('div');
+  newProfile.setAttribute('data-view', data.view);
+
+  var newHeader = document.createElement('h1');
+  newHeader.setAttribute('class', 'column-full');
+  newHeader.textContent = data.profile.fullName;
+  newProfile.appendChild(newHeader);
+
+  var newRow = document.createElement('div');
+  newRow.setAttribute('class', 'row');
+  newProfile.appendChild(newRow);
+
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', data.profile.avatarUrl);
+  newImage.setAttribute('class', 'column-half contain padding-bottom');
+  newImage.setAttribute('alt', 'Profile picture');
+  newRow.appendChild(newImage);
+
+  var newColumn = document.createElement('div');
+  newColumn.setAttribute('class', 'column-half flex-column');
+  newRow.appendChild(newColumn);
+
+  var usernameText = document.createElement('p');
+  usernameText.setAttribute('class', 'padding-bottom');
+  usernameText.textContent = data.profile.username;
+  newColumn.appendChild(usernameText);
+
+  var locationText = document.createElement('p');
+  locationText.setAttribute('class', 'padding-bottom');
+  locationText.textContent = data.profile.location;
+  newColumn.appendChild(locationText);
+
+  var bioText = document.createElement('p');
+  bioText.textContent = data.profile.bio;
+  newColumn.appendChild(bioText);
+  return newProfile;
+}
+getProfileDataInDOM();
+
 avatarUrlInput.addEventListener('input', changePicture);
 form.addEventListener('submit', submitValues);
 window.addEventListener('beforeunload', setItemsInStorage);
