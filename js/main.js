c178b8cf6a6a058189521c151636cf663d28ba24
Chris Gitmed <cgitmed@gmail.com>
2020-10-27 10:59:01 -0700

    Define a JavaScript view swapping function that takes the name of which data-view to show and hides all others. Make the view swapping function also write the name of the view it showed to the application's data model.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
-var avatarUrlInput = document.querySelector('#avatar-URL');
-var avatarImage = document.querySelector('img');
-var form = document.querySelector('form');
+var $avatarUrlInput = document.querySelector('#avatar-URL');
+var $avatarImage = document.querySelector('img');
+var $form = document.querySelector('form');
+var $viewList = document.querySelectorAll('main > div');
 
 function changePicture(event) {
-  avatarImage.src = event.target.value;
+  $avatarImage.src = event.target.value;
 }
 
 function submitValues(event) {
   event.preventDefault();
-  data.profile.avatarUrl = form.elements.avatarUrl.value;
-  data.profile.username = form.elements.username.value;
-  data.profile.fullName = form.elements.fullName.value;
-  data.profile.location = form.elements.location.value;
-  data.profile.bio = form.elements.bio.value;
-  form.reset();
-  avatarImage.src = 'images/placeholder-image-square.jpg';
+  data.profile.avatarUrl = $form.elements.avatarUrl.value;
+  data.profile.username = $form.elements.username.value;
+  data.profile.fullName = $form.elements.fullName.value;
+  data.profile.location = $form.elements.location.value;
+  data.profile.bio = $form.elements.bio.value;
+  $form.reset();
+  $avatarImage.src = 'images/placeholder-image-square.jpg';
 }
 
 function setItemsInStorage(event) {
@@ -23,45 +24,57 @@ function setItemsInStorage(event) {
 }
 
 function getProfileDataInDOM(data) {
-  var newProfile = document.createElement('div');
-  newProfile.setAttribute('data-view', data.view);
+  var $newProfile = document.createElement('div');
+  $newProfile.setAttribute('data-view', data.view);
 
-  var newHeader = document.createElement('h1');
-  newHeader.setAttribute('class', 'column-full');
-  newHeader.textContent = data.profile.fullName;
-  newProfile.appendChild(newHeader);
+  var $newHeader = document.createElement('h1');
+  $newHeader.setAttribute('class', 'column-full');
+  $newHeader.textContent = data.profile.fullName;
+  $newProfile.appendChild($newHeader);
 
-  var newRow = document.createElement('div');
-  newRow.setAttribute('class', 'row');
-  newProfile.appendChild(newRow);
+  var $newRow = document.createElement('div');
+  $newRow.setAttribute('class', 'row');
+  $newProfile.appendChild($newRow);
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', data.profile.avatarUrl);
-  newImage.setAttribute('class', 'column-half contain padding-bottom');
-  newImage.setAttribute('alt', 'Profile picture');
-  newRow.appendChild(newImage);
+  var $newImage = document.createElement('img');
+  $newImage.setAttribute('src', data.profile.avatarUrl);
+  $newImage.setAttribute('class', 'column-half contain padding-bottom');
+  $newImage.setAttribute('alt', 'Profile picture');
+  $newRow.appendChild($newImage);
 
-  var newColumn = document.createElement('div');
-  newColumn.setAttribute('class', 'column-half flex-column');
-  newRow.appendChild(newColumn);
+  var $newColumn = document.createElement('div');
+  $newColumn.setAttribute('class', 'column-half flex-column');
+  $newRow.appendChild($newColumn);
 
-  var usernameText = document.createElement('p');
-  usernameText.setAttribute('class', 'padding-bottom');
-  usernameText.textContent = data.profile.username;
-  newColumn.appendChild(usernameText);
+  var $usernameText = document.createElement('p');
+  $usernameText.setAttribute('class', 'padding-bottom');
+  $usernameText.textContent = data.profile.username;
+  $newColumn.appendChild($usernameText);
 
-  var locationText = document.createElement('p');
-  locationText.setAttribute('class', 'padding-bottom');
-  locationText.textContent = data.profile.location;
-  newColumn.appendChild(locationText);
+  var $locationText = document.createElement('p');
+  $locationText.setAttribute('class', 'padding-bottom');
+  $locationText.textContent = data.profile.location;
+  $newColumn.appendChild($locationText);
 
-  var bioText = document.createElement('p');
-  bioText.textContent = data.profile.bio;
-  newColumn.appendChild(bioText);
-  return newProfile;
+  var $bioText = document.createElement('p');
+  $bioText.textContent = data.profile.bio;
+  $newColumn.appendChild($bioText);
+  return $newProfile;
 }
-getProfileDataInDOM();
+getProfileDataInDOM(data);
 
-avatarUrlInput.addEventListener('input', changePicture);
-form.addEventListener('submit', submitValues);
+function viewSwapper(dataView) {
+  for (var i = 0; i < $viewList.length; i++) {
+    if ($viewList[i].getAttribute('data-view') !== dataView) {
+      $viewList[i].className = 'hidden';
+    } else {
+      $viewList[i].className = '';
+    }
+  }
+  data.view = dataView;
+}
+viewSwapper('profile');
+
+$avatarUrlInput.addEventListener('input', changePicture);
+$form.addEventListener('submit', submitValues);
 window.addEventListener('beforeunload', setItemsInStorage);
